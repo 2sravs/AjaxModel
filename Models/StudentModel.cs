1b3c34ccd4e1a64e77e0aac77d35c57b08174277
2Sravs <sravani.chekuri1243#gmail.com>
2023-09-04 18:24:03 +0530

    firstcommit

---
+++ Models/StudentModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Ajax_enabled.Models
+{
+    public class StudentModel
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Class { get; set; }
+        public string Email { get; set; }
+    }
+}
